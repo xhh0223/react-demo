41be82b51e9ae4df81ffb43c0513d6e2b4aa45ac
xhh0409 <xhh0409@outlook.com>
2023-03-06 23:49:34 +0800

    feat

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,31 +1,69 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import {
+    forwardRef,
+    useEffect,
+    useImperativeHandle,
+    useMemo,
+    useRef,
+    useState,
+} from "react";
 
+const Card = forwardRef(({ data }: { data: string[] }, ref) => {
+    const dataRef = useRef(new Map());
 
+    const [index, setIndex] = useState<string>();
+    useImperativeHandle(ref, () => {
+        return {
+            setSelected(index: string) {
+                debugger;
+                dataRef.current.get(index).click();
+            },
+        };
+    });
+    return (
+        <div>
+            {data.map((item) => {
+                return (
+                    <div
+                        ref={(ele) => dataRef.current.set(item, ele)}
+                        style={{ color: index === item ? "red" : "blue" }}
+                        key={item}
+                        onClick={() => {
+                            setIndex(item);
+                        }}
+                    >
+                        {item}
+                    </div>
+                );
+            })}
+        </div>
+    );
+});
 
-let obj: any[] = []
-let key =-1
-let indexStack = []
-
-function memo(val: number){
-  indexStack.push(key++)
-  obj.push(()=>{
-    let init = val
-    obj[key]
-    function setInit(v:any){
-      init =v 
-    }
-  })
-}
-memo(1)
-memo(2)
-
-const context=(v:any)=>{
-  
-}
-
-export const index = ()=>{
-  memo(1)
-  memo(2)
-  return <div></div>
-}
+export const index = () => {
+    const [data, setData] = useState(["1", "2", "3"]);
+    const cardRef = useRef<any>();
+    return (
+        <div>
+            <ul>
+                <li
+                    onClick={async () => {
+                        await new Promise((r) => {
+                            setData((s) => {
+                                r();
+                                return ["11", "22", "33"];
+                            });
+                        }).then(() => {
+                            cardRef.current.setSelected("11");
+                        });
+                    }}
+                >
+                    获取数据
+                </li>
+                <li>
+                    <Card ref={cardRef} data={data} />
+                </li>
+            </ul>
+        </div>
+    );
+};
 export default index;
