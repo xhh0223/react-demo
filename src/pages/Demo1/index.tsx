df750c6c32e325f1698f56e6d3cc6e3ce7f22f19
xuhuihui <xhh0409@outlook.com>
2023-07-05 10:18:16 +0800

    init

--- src/pages/Demo1/index.tsx
+++ src/pages/Demo1/index.tsx
@@ -1,129 +1,64 @@
-import { Dispatch, useRef, useState } from "react";
+import React, {
+    createContext,
+    useContext,
+    useEffect,
+    useId,
+    useMemo,
+    useState,
+} from "react";
 
-class Component {
-    private setState!: Dispatch<React.SetStateAction<typeof this.state>>;
-    private state: any;
-    private ref?: React.MutableRefObject<any>;
-    show() {
-        this.setState({
-            ...this.state,
-            visible: true,
-        });
-    }
-    hide() {
-        this.setState({
-            ...this.state,
-            visible: false,
-        });
-    }
-    loading() {
-        this.setState({
-            ...this.state,
-            loading: true,
-        });
-    }
-    unLoading() {
-        this.setState({
-            ...this.state,
-            loading: false,
-        });
-    }
-    render: React.FC<any> = (props) => {
-        const [state, setState] = useState({
-            visible: false,
-            loading: false,
-        });
-        this.state = state;
-        this.setState = setState;
-        this.ref = useRef<any>();
-        return (
-            <div ref={this.ref}>
-                <div>loading:{JSON.stringify(state.loading)}</div>
-                <div>visible:{JSON.stringify(state.visible)}</div>
-                <div>props:{JSON.stringify(props)}</div>
-            </div>
-        );
-    };
-}
+const FormContext = createContext<any>({});
 
-// 每次new一个实例避免了闭包问题，可以直接调用实例方法改变状态，减少不必要的函数重新调用，
-// 调用简单,理解起来也简单
-const C1 = new Component();
-const C2 = new Component();
-const Demo1 = () => {
-    const [count, setState] = useState(0);
+const Form = ({ children }: any) => {
+    const store = useMemo(() => {
+        return {
+            fields: new Map(),
+        };
+    }, []);
     return (
-        <div>
-            <div
-                onClick={() => {
-                    setState(Math.random());
-                }}
-            >
-                给个随机数
-            </div>
-            <hr />
-            <div
-                onClick={() => {
-                    C1.show();
-                }}
-            >
-                showC1
-            </div>
-            <div
-                onClick={() => {
-                    C1.hide();
-                }}
-            >
-                hideC1
-            </div>
-            <div
-                onClick={() => {
-                    C1.loading();
-                }}
-            >
-                loadingC1
-            </div>
-            <div
-                onClick={() => {
-                    C1.unLoading();
-                }}
-            >
-                unloadingC1
-            </div>
-            <hr />
-            <C1.render xhh={{ number: count }} skill={["react", "ts"]} />
-            <hr />
-            <div
-                onClick={() => {
-                    C2.show();
-                }}
-            >
-                showC2
-            </div>
-            <div
-                onClick={() => {
-                    C2.hide();
-                }}
-            >
-                hideC2
-            </div>
-            <div
-                onClick={() => {
-                    C2.loading();
-                }}
-            >
-                loadingC2
-            </div>
-            <div
-                onClick={() => {
-                    C2.unLoading();
-                }}
-            >
-                unloadingC2
-            </div>
-            <hr />
-            <C2.render xhh={{ number: count + "---" }} skill={["ax", "js"]} />
-        </div>
+        <FormContext.Provider value={store}>{children}</FormContext.Provider>
+    );
+};
+
+const Filed = ({ children, setExtraProps = () => ({}) }: any) => {
+    const { fields } = useContext(FormContext);
+    const id = useId();
+    useEffect(() => {
+        fields.set(id, undefined);
+        console.log(JSON.stringify([...fields.entries()]));
+        return () => {
+            fields.delete(id);
+        };
+    }, []);
+    return React.isValidElement(children)
+        ? React.cloneElement(children, {
+              onClick(e) {
+                  children.props.onClick(e);
+              },
+              ...setExtraProps(),
+          })
+        : children;
+};
+
+const Demo1 = () => {
+    const [state, setState] = useState(true);
+
+    return (
+        <Form>
+            {state && (
+                <Filed
+                    setExtraProps={() => {
+                        return {
+                            style: {
+                                color: "red",
+                            },
+                        };
+                    }}
+                >
+                    <div>22132</div>
+                </Filed>
+            )}
+        </Form>
     );
 };
 export default Demo1;
--- src/pages/Demo7/index.tsx
+++ src/pages/Demo7/index.tsx
@@ -1,28 +1,6 @@
-import Form, { Field } from "rc-field-form";
 import React from "react";
-const Input = ({ value = "", ...props }) => <input value={value} {...props} />;
-const Demo6 = () => {
-    return (
-        <div>
-            <Form
-                onFinish={(values) => {
-                    console.log("Finish:", values);
-                }}
-            >
-                <Field
-                    name="username"
-                    rules={[{ required: true, message: "122" }]}
-                >
-                    <Input placeholder="Username" />
-                </Field>
-                <br />
-                <Field name="password">
-                    <Input placeholder="Password" />
-                </Field>
-                <button>Submit</button>
-            </Form>
-        </div>
-    );
+const Demo7 = () => {
+    return <div></div>;
 };
 
-export default Demo6;
+export default Demo7;
